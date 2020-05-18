d28b62cb716a5b86c70e24aed59beb6d3d2a7896
sushantadey1 <sushanta@iweensoft.com>
2020-05-18 15:49:35 +0530

    Updates

---
+++ precache-manifest.ce36cbdc6c379c406e1c52185f773565.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17aa8d584d757b4ed78bfcbc0332a1a5",
+    "url": "/nsc_v1/index.html"
+  },
+  {
+    "revision": "8c217b4e43b8aace098c",
+    "url": "/nsc_v1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3cba9e1a9bc63dbcb42b",
+    "url": "/nsc_v1/static/js/2.6dc1e129.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nsc_v1/static/js/2.6dc1e129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c217b4e43b8aace098c",
+    "url": "/nsc_v1/static/js/main.7cc971f9.chunk.js"
+  },
+  {
+    "revision": "b71e05f4b1c6c6b5fb50",
+    "url": "/nsc_v1/static/js/runtime-main.da207c37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nsc_v1/static/media/logo.5d5d9eef.svg"
+  }
+]);
